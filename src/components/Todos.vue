43072e42c58cdb92cd98191d99ffcb08648689cf
mr-fool <mr-fool@users.noreply.github.com>
2019-12-07 17:07:58 -0700

    bug

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,11 +1,14 @@
 <template>
    <div>
-       <h1>Todos</h1>
+        <div v-for="todo in todos">
+            <h3>{{todo.title}}</h3>
+        </div>
    </div> 
 </template>
 <script>
 export default {
-    name: "Todos"
+    name: "Todos",
+    props: ["todos"]
 }
 </script>
 <style scoped>
