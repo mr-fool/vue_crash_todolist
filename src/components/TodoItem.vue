a603116f91a1fb54635e38b0173c9ddfb8d20a02
mr-fool <mr-fool@users.noreply.github.com>
2019-12-08 17:13:42 -0700

    no console error

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,11 +1,20 @@
 <template>
-    <div>
-        <p>Todo Item</p>
+    <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+        <p>
+            <input type="checkbox" v-on:change="markComplete">
+            {{todo.title}}
+        </p>
     </div>
 </template>
 <script>
 export default {
-    
+    name: "TodoItem",
+    props: ["todo"],
+    methods: {
+        markComplete() {
+            console.log(123)
+        }
+    }
 }
 
 </script>
